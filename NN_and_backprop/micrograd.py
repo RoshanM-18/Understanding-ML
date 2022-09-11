fec26485a36883753138dfff05a34d258f6bb3ff
RoshanM-18 <roshanmaur@hotmail.com>
2022-09-11 22:42:53 +0530

    updating micrograd.py

--- NN_and_backprop/micrograd.py
+++ NN_and_backprop/micrograd.py
@@ -1,17 +1,28 @@
+import math
 import numpy as np
 
 class Value:
 
-    def __init__(self, data):
+    def __init__(self, data, _children=(), _op="", label=""):
         self.data = data
+        self.grad = 0.0  # 0 means no effect meaning we assume that at init it doesnt affect the output
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
 
     def __repr__(self):
         return f"Value(data={self.data})"
 
     def __add__(self, other):
-        output = self.data + other.data
+        output = Value(self.data + other.data, (self, other), "+")
         return output
 
     def __mul__(self, other):
-        output = self.data * other.data
+        output = Value(self.data * other.data, (self, other), "*")
         return output
+
+    def tanh(self):
+        n = self.data
+        t = (math.exp(2*n)-1)/(math.exp(2*n)+1)
+        out = Value(t, _children=(self, ), _op="tanh")
+        return out
